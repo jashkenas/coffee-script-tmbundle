533bf798aae09b3e2476643a1c2eb269c86643c9
Lorin Tackett <lorin.tackett@gmail.com>
2011-09-28 17:33:27 -0700

    Added syntax highlighting to outputted javascript

--- coffee.py
+++ coffee.py
@@ -22,7 +22,8 @@ class CoffeeCompileCommand(sublime_plugin.TextCommand):
 
     def output_to_new_view(self, output):
         new_view = sublime.active_window().new_file()
-        new_view.set_name("(compiled CoffeeScript)")
+        new_view.set_name("compiled_CoffeeScript.js")
+        new_view.set_syntax_file("Packages/JavaScript/JavaScript.tmLanguage")
         new_view.set_scratch(True)
         edit = new_view.begin_edit()
         new_view.insert(edit, 0, output)
